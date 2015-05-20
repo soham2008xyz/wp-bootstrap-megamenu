6ddde3e5315f8ae7acb319497a2a9bfea847dc3c
narsariamanu <narsariamanu@users.noreply.github.com>
2015-05-21 01:10:38 +0530

    test 3

--- megamenu.js
+++ megamenu.js
@@ -1,7 +1,7 @@
 $(document).on('click',".megamenu .dropdown-menu",function (e){
   console.log('dropdown-menu');
   $(this).parent().addClass("open");
-  $(this).parent().find(".dropdown-toggle").attr
+  $(this).parent().find(".dropdown-toggle").attr("aria-expanded","true");
   e.stopPropagation();
 });
 
