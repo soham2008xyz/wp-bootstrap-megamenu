732712c60e3252a481c6eac11f7e7f516b7aaf1e
Soham Banerjee <soham2008xyz@gmail.com>
2016-12-18 23:43:36 +0530

    Bugfix in IEM Group Website

--- megamenu.js
+++ megamenu.js
@@ -1,6 +1,8 @@
-$(document).on('click',".megamenu .dropdown-menu",function (e){
-  //console.log('dropdown-menu');
-  $(this).parent().addClass("open");
-  $(this).parent().find(".dropdown-toggle").attr("aria-expanded","true");
-  e.stopPropagation();
-});
+( function($) {
+    $(document).on('click',".megamenu .dropdown-menu",function (e){
+        //console.log('dropdown-menu');
+        $(this).parent().addClass("open");
+        $(this).parent().find(".dropdown-toggle").attr("aria-expanded","true");
+        e.stopPropagation();
+    });
+} )(jQuery);
